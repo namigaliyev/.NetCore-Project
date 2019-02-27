c19ac2688a1c84249a4a929bb977ddf4c0811a40
Namig Aliyev <namig.aliyev@polisoft.com.tr>
2019-02-27 15:29:14 +0300

    Fixed

--- NewsSystem.Admin/Startup.cs
+++ NewsSystem.Admin/Startup.cs
@@ -4,14 +4,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-<<<<<<< HEAD
-
-=======
-using NewsSystem.Data.DataContext;
-using Autofac;
-using Autofac.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
->>>>>>> 8c2dd74f6bf7b628cc081114ca58de1060e0108d
 
 namespace NewsSystem.Admin
 {
@@ -30,58 +22,22 @@ namespace NewsSystem.Admin
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public IServiceProvider ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services)
         {
-<<<<<<< HEAD
-=======
-
-            #region Other DI Configuration
-            var sqlConnectionString = Configuration.GetConnectionString("DefaultConnection");
- 
-            services.AddDbContext<NewsContext>(options =>
-                options.UseMySql(
-                    sqlConnectionString,
-                    b => b.MigrationsAssembly("AspNetCoreMultipleProject")
-                )
-            );
-
-            services.AddSingleton(Configuration);
-
->>>>>>> 8c2dd74f6bf7b628cc081114ca58de1060e0108d
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-
-<<<<<<< HEAD
             services.AddScoped(typeof(IBaseRepository<>),typeof(BaseRepository<>));
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-=======
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
-            #endregion
-
-            var builder = new Autofac.ContainerBuilder();
-            builder.Populate(services);
-            var container = builder.Build();
-            return container.Resolve<IServiceProvider>();
-
->>>>>>> 8c2dd74f6bf7b628cc081114ca58de1060e0108d
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(
-            IApplicationBuilder app,
-            IHostingEnvironment env,
-            ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-
-            loggerFactory.AddConsole(this.Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
